e1e4b20355a67698d8aa51dc4ab3bfdd94e7c727
unknown <brunoalvesdasilvabr@gmail.com>
2019-05-27 16:39:40 -0300

    CSS Pag. Navegação concertado

--- application/views/template/login.php
+++ application/views/template/login.php
@@ -1,21 +1,21 @@
 
 <div class="bodylogin">
-<div class="containerLogin">
-   <div class="containersvg">
-        <img class="loginSvg" src="./public/src/dev/img/user.svg" alt="login icon">
-   </div>
+    <div class="containerLogin">
+        <div class="containersvg">
+                <img class="loginSvg" src="./public/src/dev/img/user.svg" alt="login icon">
+        </div>
 
-   <form>
-       <div class="containerInput">
-             <span><img  class="loginSvgInput" src="./public/src/dev/img/user.svg"/></span><input type="text" name="username" placeholder="USERNAME"/>
-       </div>
-       <div class="containerInput">
-             <span><img class="loginSvgInput" src="./public/src/dev/img/unlocked.svg"/></span><input type="password" name="password" placeholder="PASSWORD"/>
-      </div>
-       <button class="btnlogin" type="submit">LOGIN</button>
-       <div class="check">
-           <input id="remember" class="checkInput" type="checkbox"/> <label for="remember">Remember me </label> <p>Forgot your password</p>
-       </div>
-  </form>
-</div>
+        <form>
+            <div class="containerInput">
+                    <span><img  class="loginSvgInput" src="./public/src/dev/img/user.svg"/></span><input type="text" name="username" placeholder="USERNAME"/>
+            </div>
+            <div class="containerInput">
+                    <span><img class="loginSvgInput" src="./public/src/dev/img/unlocked.svg"/></span><input type="password" name="password" placeholder="PASSWORD"/>
+            </div>
+            <button class="btnlogin" type="submit">LOGIN</button>
+            <div class="check">
+                <input id="remember" class="checkInput" type="checkbox"/> <label for="remember">Remember me </label> <p>Forgot your password</p>
+            </div>
+        </form>
+    </div>
 </div>
