41ab7b2a7b81625a435d09de663e87e9d28b830d
igor-houpa <igor.bezerra@houpa.app>
2019-05-29 15:49:15 -0300

    Responsividade, Pagina de Listagem de Usuários

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,8 +1,6 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script> 
-	<script src="./public/src/dev/js/libs/projeto.js"></script>
 	<script src="./public/src/dev/js/libs/angular.js"></script>
-	<script src="./public/src/dev/js/libs/Chart.min.js"></script>
 	<script src="./public/src/dev/angular/module/module.js"></script>
 	<script src="./public/src/dev/angular/controllers/controller.js"></script>
 	<script type="text/javascript" src="./public/src/dev/js/libs/waves.min.js"></script>
--- public/src/dev/angular/controllers/controller.js
+++ public/src/dev/angular/controllers/controller.js
@@ -1,85 +1,36 @@
 // Controller
+
 app.controller('appCtrl', ['$scope', function($scope) {
+
+    //ABERTURA DO MODAL
     $scope.modalView = false;
 
     $scope.modal = function(){
         $scope.modalView = !$scope.modalView;
     }
-        var friends = [
-            {img:'img.jpg', name:'Peter'},
-            {img:'img.jpg', name:'Peter'},
-            {img:'img.jpg', name:'Peter'},
-            {img:'img.jpg', name:'Peter'}
-            
-        ];
+
+
+    //REPEAT DO CARD
+    var friends = [
+        {img:'img.jpg', name:'Peter'},
+        {img:'img.jpg', name:'Peter'},
+        {img:'img.jpg', name:'Peter'},
+        {img:'img.jpg', name:'Peter'},
+        {img:'img.jpg', name:'Peter'},
+        {img:'img.jpg', name:'Peter'},
+        {img:'img.jpg', name:'Peter'}
+    ];
+
     $scope.reset = function() {
         $scope.friends = angular.copy(friends);
-    };
+    }     
     $scope.reset();
     
+    //MENU HAMBURGUER
+    $scope.hamburguerMenu = false;
+
+    $scope.hamburguer = function(){
+
+        $scope.hamburguerMenu = !$scope.hamburguerMenu;
+    }
 }]);
-
-$('.checkbox').click(function(){
-    var verificar = $(this).attr('verificar');
-    if(verificar == 0){
-        $('.checkbox').removeClass('fa-square').addClass('fa-check-square');
-        $('.checkbox').attr('verificar', 1);
-        $("#remember").click();
-    }else{
-        $('.checkbox').removeClass('fa-check-square').addClass('fa-square');
-        $('.checkbox').attr('verificar', 0);
-        $("#remember").click();
-    }
-})
-
-
-
-var ctx = document.getElementById('myChart').getContext('2d');
-
-var myChart = new Chart(ctx, {
-    type: 'bar',
- 
-    data: {
-        labels: ['jan', 'feb', 'Mar', 'Apr', 'May', 'June', 'July', 'August', 'Sep', 'Oct', 'Nov', 'Dec'],
-
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3,12, 5, 3, 5, 2, 3],
-            backgroundColor: [
-                '#fe9500',
-                '#fe9500',
-                '#fe9500',
-                '#fe9500',
-                '#fe9500',
-                '#fe9500',
-                '#fe9500',
-                '#fe9500',
-                '#fe9500',
-                '#fe9500',
-                '#fe9500',
-                '#fe9500'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    
-    options: {
-
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
